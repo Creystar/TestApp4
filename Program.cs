b5b6660fe83cbb5b1227b9d7b377345f18c06d7f
Konastantin Lishavsky <lishavsky95@mail.ru>
2024-09-18 21:02:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+
+class EnteringInteger
+{
+    static void Main()
+    {
+        //Текстовые переменные
+        string res, txt;
+
+        //Целочисленные переменные
+        int year = 2024, age, born;
+
+        //Отображение окна с полем ввода:
+        res = Interaction.InputBox("В каком году вы родились?","Год рождения");
+
+        //Преобразование текста в число:
+        born=short.Parse(res);
+
+        //Вычисление возраста:
+        age=year-born;
+        txt = "Тогда вам " + age + " лет";
+
+        //Окно с сообщением
+        MessageBox.Show(txt, "Возраст");
+    }
+}
